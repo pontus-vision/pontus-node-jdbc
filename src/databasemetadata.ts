f468c7761d1b09e63848efa4e6206ddc5cc4c94e
Leonardo Martins <lmartins@pontusnetworks.com>
2024-08-26 16:28:34 +0100

    Create databasemetadata.ts

---
+++ src/databasemetadata.ts
@@ -0,0 +1,95 @@
+import _ from 'lodash';
+import { ResultSet } from './resultset';
+import { Connection } from './connection';
+import { jinst } from './jinst';
+
+const java = jinst.getInstance();
+
+export class DatabaseMetaData {
+  private _dbm: any;
+
+  constructor(dbm: any) {
+    this._dbm = dbm;
+  }
+
+  async getSchemas(catalog?: string, schemaPattern?: string): Promise<ResultSet> {
+    const validParams = (
+      (_.isNull(catalog) || _.isUndefined(catalog) || _.isString(catalog)) &&
+      (_.isNull(schemaPattern) || _.isUndefined(schemaPattern) || _.isString(schemaPattern))
+    );
+
+    if (!validParams) {
+      throw new Error('INVALID ARGUMENTS');
+    }
+
+    return new Promise<ResultSet>((resolve, reject) => {
+      this._dbm.getSchemas(catalog, schemaPattern, (err: Error | null, result: any) => {
+        if (err) reject(err);
+        else resolve(new ResultSet(result));
+      });
+    });
+  }
+
+  async getTables(catalog?: string, schemaPattern?: string, tableNamePattern?: string, types?: string[]): Promise<ResultSet> {
+    const validParams = (
+      (_.isNull(catalog) || _.isUndefined(catalog) || _.isString(catalog)) &&
+      (_.isNull(schemaPattern) || _.isUndefined(schemaPattern) || _.isString(schemaPattern)) &&
+      (_.isNull(tableNamePattern) || _.isUndefined(tableNamePattern) || _.isString(tableNamePattern)) &&
+      (_.isNull(types) || _.isUndefined(types) || _.isArray(types))
+    );
+
+    if (_.isArray(types)) {
+      for (const type of types) {
+        if (!_.isString(type)) {
+          throw new Error('INVALID ARGUMENTS');
+        }
+      }
+    }
+
+    if (!validParams) {
+      throw new Error('INVALID ARGUMENTS');
+    }
+
+    return new Promise<ResultSet>((resolve, reject) => {
+      this._dbm.getTables(catalog, schemaPattern, tableNamePattern, types, (err: Error | null, result: any) => {
+        if (err) reject(err);
+        else resolve(new ResultSet(result));
+      });
+    });
+  }
+
+  async allProceduresAreCallable(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this._dbm.allProceduresAreCallable((err: Error | null, result: boolean) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  }
+
+  async allTablesAreSelectable(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this._dbm.allTablesAreSelectable((err: Error | null, result: boolean) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  }
+
+  async autoCommitFailureClosesAllResultSets(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this._dbm.autoCommitFailureClosesAllResultSets((err: Error | null, result: boolean) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  }
+
+  // ... Continue with the rest of the methods, converting them to async/await ...
+
+  // Example of a more complex method conversion:
+  async getAttributes(catalog?: string, schemaPattern?: string, typeNamePattern?: string, attributeNamePattern?: string): Promise<ResultSet> {
+    const validParams = (
+      (_.isNull(catalog) || _.isUndefined(catalog) || _.isString(catalog)) &&
+      (_.isNull(schemaPattern) || _.isUndefined(schemaPattern) || _.isString(schemaPattern)) &&
+      (_.isNull(type
