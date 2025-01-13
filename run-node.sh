a4861ccf7deaa3bf08abfab584ac2d03001a82f3
foobar <pvpmartins@hotmail.com>
2025-01-13 17:31:29 +0100

    pvhome-#25: exporting CLASSPATH properly

--- run-node.sh
+++ run-node.sh
@@ -4,6 +4,6 @@ set -e
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$DIR"
 
-export CLASSPATH="$(ls $(pwd)/jars/*|xargs|sed -e 's/ /,/g' )"
+export CLASSPATH="$(ls $(pwd)/delta-table/node/jars/*|xargs|sed -e 's/ /,/g' )"
 
 
